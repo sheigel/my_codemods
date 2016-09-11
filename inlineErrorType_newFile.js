1e343f41b13649306ab91488172bf4b6ee0ff193
Silviu Eigel <sheigel@sheigel.local>
2016-09-11 22:23:58 +0300

    refactoring

--- inlineErrorType_newFile.js
+++ inlineErrorType_newFile.js
@@ -6,13 +6,6 @@ export default function transformer(file, api, options) {
     const {expression, statement, statements} = j.template
     const sourceRoot = j(file.source)
 
-    let destFilePath = `${path.dirname(file.path)}/output.js`
-
-    fs.existsSync(destFilePath) || fs.writeFileSync(destFilePath, "")
-    let destFile = {path: destFilePath, source: fs.readFileSync(destFilePath).toString()}
-
-    let destRoot = j(destFile.source)
-
     let identifyErrorType = () => {
         return sourceRoot
             .find(j.FunctionDeclaration)
@@ -23,14 +16,13 @@ export default function transformer(file, api, options) {
                     .find(j.ExpressionStatement, {
                         expression: {
                             type: j.AssignmentExpression.name,
-                            left: {object: {name: funcDecl.node.id.name}}
+                            left: { object: { name: funcDecl.node.id.name } }
                         }
                     })
                     .paths()
                 if (!funcProtoAssignment.length) {
                     return null
                 }
-
                 return {
                     funcDecl,
                     funcProtoAssignment: funcProtoAssignment[0]
@@ -39,34 +31,41 @@ export default function transformer(file, api, options) {
             .filter(t => t)
 
     }
-    let copyErrorTypesTo = (errorTypes, destRoot) =>
-        errorTypes
-            .map(pathsOfType => ({
-                funcDecl: pathsOfType.funcDecl.node, funcProtoAssignment: pathsOfType.funcProtoAssignment.node
-            }))
-            .forEach(t => {
-                destRoot
-                    .find(j.Program)
-                    .replaceWith(p => {
-                        return j.program([
-                            ...p.node.body,
-                            t.funcDecl,
-                            t.funcProtoAssignment
-                        ])
-                    })
+    let copyErrorTypesTo = (errorTypes, destRoot) => errorTypes
+        .map(pathsOfType => ({
+            funcDecl: pathsOfType.funcDecl.node, funcProtoAssignment: pathsOfType.funcProtoAssignment.node
+        }))
+        .forEach(t => {
+            destRoot
+                .find(j.Program)
+                .replaceWith(p => {
+                    return j.program([
+                        ...p.node.body,
+                        t.funcDecl,
+                        t.funcProtoAssignment
+                    ])
+                })
 
-            })
+        })
+
+    let destFilePath = `${path.dirname(file.path)}/output.js`
+
+    //create destination file if it doesn't exist
+    fs.existsSync(destFilePath) || fs.writeFileSync(destFilePath, "")
+
+    let destFile = { path: destFilePath, source: fs.readFileSync(destFilePath).toString() }
+
+    let destRoot = j(destFile.source)
 
     copyErrorTypesTo(identifyErrorType(), destRoot)
-    let destSource = destRoot.toSource(options.printOptions || {quote: 'single'})
+    let destSource = destRoot.toSource(options.printOptions || { quote: 'single' })
     fs.writeFileSync(destFile.path, destSource)
 
     //remove error type definition paths from original file
     identifyErrorType().forEach(t => {
         t.funcDecl.replace()
         t.funcProtoAssignment.replace()
-
     })
 
-    return sourceRoot.toSource(options.printOptions || {quote: 'single'})
+    return sourceRoot.toSource(options.printOptions || { quote: 'single' })
 }
